f79d65fdaafb3ff5918a0a25a0c27b42a9840a9f
Appletalk <Appletalk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-05-07 17:12:30 +0000

    Old release removed
    git-svn-id: https://plugins.svn.wordpress.org/wp-o-matic/trunk@44753 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/admin/footer.php
+++
@@ -1,7 +0,0 @@
-  </div>
-</div>
-
-<div id="wpofooter">                                                                            
-  <!-- Remove me not! -->
-  <p>WP-o-Matic <?php echo $this->version ?> &mdash; Copyright &copy;2008 <a href="http://devthought.com">Guillermo Rauch</a></p>
-</div>    
