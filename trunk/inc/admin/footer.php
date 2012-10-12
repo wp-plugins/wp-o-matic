8c5d54274f91d5aab9684214419aede48b217505
rrauch <rrauch@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-12 03:15:00 +0000

    tagging version 2.2
    git-svn-id: https://plugins.svn.wordpress.org/wp-o-matic/tags/2.2@611242 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/inc/admin/footer.php
@@ -0,0 +1,7 @@
+  </div>
+</div>
+
+<div id="wpofooter">                                                                            
+  <!-- Remove me not! -->
+	<p>WP-o-Matic <?php echo $this->version ?> &mdash; Copyright &copy;2012 <a href="http://themeskult.com">Themes Kult</a></p>
+</div>    
